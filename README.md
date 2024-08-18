f3de5e7c79c2c2ae47d6480cae1cf60d67532c14
Md. Minarul Islam <144237492+Minarulns@users.noreply.github.com>
2024-08-18 13:50:20 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,93 @@
+<p align="center">
+<a href="https://h4ck3r0.github.io/"><img title="Made in BANGLADESH" src="https://img.shields.io/badge/MADE%20IN-BANGLADESH-SCRIPT?colorA=%23ff8100&colorB=%23017e40&colorC=%23ff0000&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://h4ck3r0.github.io/"><img title="Made in INDIA" src="https://img.shields.io/badge/Tool-TermuxOs-green.svg?style=flat-square"></a>
+<a href="https://h4ck3r0.github.io/"><img title="Version" src="https://img.shields.io/badge/Version-1.0-green.svg?style=flat-square"></a>
+<a href="https://h4ck3r0.github.io/"><img title="Maintainence" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg?style=flat-square"></a>
+</p>
+<p align="center">
+ <a href=""><img src="https://user-images.githubusercontent.com/46929618/150730092-337cd5de-f376-454a-9418-c884bdb5f5e0.png" width="1000" hight="200"></a>
+</p>
+<p align="center">
+<a href="https://github.com/h4ck3r0"><img title="Github" src="https://img.shields.io/badge/H4CK3R-RAJ-brightgreen?style=for-the-badge&logo=github"></a>
+<a href="https://youtu.be/VDeLnDxVziw"><img title="YouTube" src="https://img.shields.io/badge/YouTube-H4CK3R-red?style=for-the-badge&logo=Youtube"></a>
+</p>
+<p align="center">
+<a href="https://github.com/h4ck3r0"><img title="Language" src="https://img.shields.io/badge/Made%20with-Bash-1f425f.svg?v=103&style=flat-square"></a>
+<a href="https://github.com/h4ck3r0"><img title="Followers" src="https://img.shields.io/github/followers/h4ck3r0?color=blue&style=flat-square"></a>
+<a href="https://github.com/h4ck3r0"><img title="Stars" src="https://img.shields.io/github/stars/h4ck3r0/Termux-os?color=red&style=flat-square"></a>
+<a href="https://github.com/h4ck3r0"><img title="Forks" src="https://img.shields.io/github/forks/h4ck3r0/Termux-os?color=red&style=flat-square"></a>
+<a href="https://github.com/h4ck3r0"><img title="Watching" src="https://img.shields.io/github/watchers/h4ck3r0/Termux-os?label=Watchers&color=blue&style=flat-square"></a>
+<a href="https://github.com/h4ck3r0"><img title="Licence" src="https://img.shields.io/badge/License-GNU-blue.svg?style=flat-square"></a>
+</p>
+
+## Termux-os
+
+* [+] Based on Zsh Shell !
+
+* [+] Pure Termux Look !
+
+
+## Features
+
+`Added Termux Extra Keys`
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/46929618/150729794-17dd4f17-19d0-4028-99b8-c7a88994d145.jpg" width="300" hight="220">
+</p>
+
+`Your Name Banner`
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/46929618/150729923-9f0415aa-d0fd-46c7-86cb-1ef65affffcb.jpg" width="300" hight="220">
+</p>
+
+`Added Own made Advance zsh theme`
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/46929618/150729435-7eea5d74-8474-427a-9ada-a8d50da91136.jpg" width="300" hight="220">
+</p>
+
+`Added Highlight / Autosuggestion`
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/46929618/150729854-a09be75c-5e3a-4a21-85b4-71191ee42bd2.jpg" width="300" hight="220">
+</p>
+
+## Photos Of Theme
+
+
+
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/46929618/150729988-7c0c2a39-fe78-4dc6-8174-1a220e0ee1e5.jpg" width="300" hight="220">
+</p>
+
+## Installation
+
+
+
+* `git clone https://github.com/h4ck3r0/Termux-os`
+
+* `cd Termux-os`
+
+* `bash os.sh`
+
+#### Full Process : [Video](https://youtu.be/VSG2glraCws) 
+
+## [Read More](https://www.h4ck3r.me/how-to-install-theme-in-termux-in-one-click/) 
+
+## Credits
+
+* [+] Credits to Oh My Zsh !
+* [+] H4Ck3R ( Raj Aryan )
+
+## CONNECT WITH US :
+<a href="https://www.buymeacoffee.com/h4ck3r" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+
+<a href="https://github.com/h4ck3r0"><img title="Github" src="https://img.shields.io/badge/H4Ck3R-Raj-brightgreen?style=for-the-badge&logo=github"></a>
+[![Instagram](https://img.shields.io/badge/INSTAGRAM-FOLLOW-red?style=for-the-badge&logo=instagram)](https://www.instagram.com/h4ck3r0_official?igsh=NzN1NTNucWhjOXJp)
+[![Instagram](https://img.shields.io/badge/WEBSITE-VISIT-yellow?style=for-the-badge&logo=blogger)](https://www.h4ck3r.me)
+[![Instagram](https://img.shields.io/badge/TELEGRAM-CHANNEL-red?style=for-the-badge&logo=telegram)](https://t.me/h4ck3r_group)
+<a href="https://rebrand.ly/7elzgww"><img title="YouTube" src="https://img.shields.io/badge/YouTube-H4Ck3R-red?style=for-the-badge&logo=Youtube"></a>
+https://www.h4ck3r.me/how-to-install-theme-in-termux-in-one-click/
---
+++ os.sh
@@ -0,0 +1,126 @@
+clear
+echo -e "\e[31m  _____ _____ ____  __  __ _   ___  __     __  __     _ "
+echo -e "\e[31m |_   _| ____|  _ \|  \/  | | | \ \/ /    |  \/  |   | |"
+echo -e "\e[96m   | | |  _| | |_) | |\/| | | | |\  /_____| |\/| |_  | |"  
+echo -e "\e[96m   | | | |___|  _ <| |  | | |_| |/  \_____| |  | | |_| |" 
+echo -e "\e[94m   |_| |_____|_| \_\_|  |_|\___//_/\_\    |_|  |_|\___/ "
+
+echo ""
+echo -e " \e[1;91m Youtube\e[96m / \e[100;97myoutube.com/minarulns\e[0;31m"
+echo -e " \e[1;91m Github\e[96m /\e[1;93m Minarulns"
+echo -e " \e[1;91m Telegram\e[1;96m /\e[1;92m Minarulns"
+echo ""
+
+
+banner ( ) {
+                  
+                  
+                  printf ""
+                  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91mby \e[1;36]Md. Minarul Islam\e[93m/ \e[100;92myoutube.com/minarulns\e[0m"
+                  }
+
+                  wr  ( )  {
+                               printf "\033[1;91m Invalid input!!!\n"
+                               selection
+                               }
+                               1line() {
+                                                         apt update && apt upgrade
+                                                         pkg install zsh -y
+                                                         pkg install git -y
+                                                         pkg install figlet toilet -y
+                                                         pkg install ruby  -y
+                                                         pkg install wget  -y
+                                                       
+                                                         gem install lolcat 
+                                                         pkg install curl -y
+                                                         pkg install zsh -y
+                                                         
+                                                         clear
+                                                         cd ~/Termux-os/.object/ && cp -r 'ANSI Shadow.flf'  $PREFIX/share/figlet/ASCII-Shadow.flf 
+                                                         git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+                                                       
+                                                         pkg install toilet figlet exa -y
+                                                         cd ~/Termux-os/.object
+                                                         rm -rf ~/.termux/colors.properties
+                                                         rm -rf /data/data/com.termux/files/usr/etc/motd
+                                                         cp -r .colors.properties ~/.termux/colors.properties
+                                                         cp -r .termux.properties ~/.termux/termux.properties
+                                                         clear
+                                                         cd ~/Termux-os ; bash os.sh
+                                                         termux-open-url h4ck3r.me
+                                                         
+                                                         }
+                                                         2line() {
+                                                                               rm -rf ~/.zshrc
+                                                                               git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+                                                                               cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+                                                                               cd ~/Termux-os ; bash os.sh
+                                                                                }
+                                                                               3line() {
+                                                                                                 pkg install zsh
+                                                                                                 chsh -s zsh
+                                                                                                 cd ~/Termux-os ; bash os.sh
+                                                                                                  }
+                                                                                                   4line() {
+                                                                                                                         chsh -s bash
+                                                                                                                         cd  ~/Termux-os ; bash os.sh
+                                                                                                                         }
+                                                                                                                         5line() {
+                                                                                                                                              rm -rf ~/.zshrc
+                                                                                                                                              cd ~/Termux-os/.object
+                                                                                                                                              bash  .2.sh
+                                                                                                                                              clear ; cd ~/Termux-os ; bash os.sh
+                                                                                                                                                  }
+                                                                                                                                                 
+                                                                                                                                                6line() {
+                                                                                                                                                                         cd ~/Termux-os/.object
+                                                                                                                                                                         bash .1.sh
+                                                                                                                                                                         clear ; cd ~/Termux-os ; bash os.sh
+                                                                                                                                                                       }
+                                                                                                                                                                       7line() {
+                                                                                                                                                                                                                    cd ~/Termux-os/.object
+                                                                                                                                                                                                                    rm -rf ~/.zshrc
+                                                                                                                                                                                                                    chsh -s zsh
+                                                                                                                                                                                                                    bash .3.sh
+                                                                                                                                                                                                                    clear ; cd ~/Termux-os ; bash os.sh
+                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                     8line() {
+                                                                                                                                                                                                                                  rm -rf ~/Termux-os
+                                                                                                                                                                                                                                  cd
+                                                                                                                                                                                                                                  git clone https://github.com/Minarulns/Termux-os
+                                                                                                                                                                                                                                  cd ~/Termux-os ; bash os.sh
+                                                                                                                                                                                                                                  }
+    
+                                 selection () {
+                                            cd ~/Termux-os
+                                            echo -e -n "\e[1;96m Choose\e[1;96m Option : \e[0m"
+                                            read a
+                                            case $a in
+                                            1) 1line ;;
+                                            2) 2line ;;
+                                            3) 3line ;;
+                                            4) 4line ;;
+                                            5) 5line ;;
+                                            6) 6line ;;
+                                            7) 7line ;;
+                                            8) 8line ;;
+                                            9) exit ;;
+                                            *) wr ;;
+                                            esac
+                                            }
+
+                  menu () {
+                                  banner
+                                  printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Necessary Setup \n"
+                                  printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Zsh Setup\n"
+                                  printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;92m Zsh Shell\n"
+                                  printf "\033[1;91m[\033[0m4\033[1;91m]\033[1;92m Bash Shell\n"
+                                  printf "\033[1;91m[\033[0m5\033[1;91m]\033[1;92m Zsh Banner\n"
+                                  printf "\033[1;91m[\033[0m6\033[1;91m]\033[1;92m Zsh Theme\n"
+                                  printf "\033[1;91m[\033[0m7\033[1;91m]\033[1;92m Highlight / AutoSuggest\n"
+                                  printf "\033[1;91m[\033[0m8\033[1;91m]\033[1;92m Update\n"
+                                  printf "\033[1;91m[\033[0m9\033[1;91m]\033[1;92m Exit\n\n\n"
+                                  
+                                  selection
+                                  }
+                  menu
