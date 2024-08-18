7cdb9d59d2f53f74be91a6788c83f06d088c5dfb
Md. Minarul Islam <144237492+Minarulns@users.noreply.github.com>
2024-08-18 13:51:34 +0600

    Add files via upload

---
+++ .object/.3.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /data/data/com.termux/files/home/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+clear
+PUT(){ echo -en "\033[${1};${2}H";}
+clear;toilet -t -f mono12  "HACKER" --gay -F border
+echo ""
+PUT 11 40
+echo -e "\e[92mBy\e[1;93m Minarul Islam\e[1;95m /\e[1;96m  "
+PUT 12 3
+echo ""
+echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mMd. Minarul Islam \e[93m/ \e[100;92myoutube.com/minarulns\e[0m"
+echo
+echo -e "  \e[101;1;39mNOTE\e[0;1;33m Use upto 9 words\e[0m"
+echo ""
+cd ~/Termux-os/.object
+echo -e '\e[1;96m'
+read -p '  Type Banner Name ❯ ' name
+sed -e "s/\PROC/$name/g" .2zshrc > $HOME/.zshrc
+echo  ""
+echo -e '\e[1;96m'
+read -p '  Type Shell Name ❯ ' name
+sed -e "s/\H4ck3r/$name/g" .h4Ck3r.zsh-theme > $HOME/.oh-my-zsh/themes/h4Ck3r.zsh-theme
---
+++ .object/.h4Ck3r.zsh-theme
@@ -0,0 +1,12 @@
+local current_dir='%{$fg_bold[red]%}[%{$reset_color%}%~% %{$fg_bold[red]%}]%{$reset_color%}'
+local git_branch='$()%{$reset_color%}'
+
+
+PROMPT="
+%(?,%{$fg_bold[red]%} ┌─╼%{$fg_bold[red]%}[%{$fg_bold[blue]%}MINARUL💙JANNAT%{$fg_bold[yellow]%}%{$fg_bold[cyan]%}%{$fg_bold[red]%}]%{$fg_bold[green]%}-%{$fg_bold[red]%}[%{$fg_bold[green]%}%(5~|%-1~/…/%2~|%4~)%{$fg_bold[red]%}]%{$reset_color%} ${git_branch}
+%{$fg_bold[red]%} └────╼%{$fg_bold[white]%} ❯%{$fg_bold[blue]%}❯%{$fg_bold[cyan]%}❯%{$reset_color%} ,%{$fg_bold[red]%} ┌─╼%{$fg_bold[red]%}[%{$fg_bold[green]%}%(5~|%-1~/…/%2~|%4~)%{$fg_bold[red]%}]%{$reset_color%}
+%{$fg_bold[red]%} └╼%{$fg_bold[white]%} ❯%{$fg_bold[blue]%}❯%{$fg_bold[cyan]%}❯%{$reset_color%} "
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}["
+ZSH_THEME_GIT_PROMPT_SUFFIX="] %{$reset_color%}"
+
